60f41131b954596867db1a942ecfdaef0156a448
Marco Perez <108315533+VinicioPerez@users.noreply.github.com>
2024-08-01 13:49:30 -0500

    terminadno el proyecto

---
+++ src/components/Alerta.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="alerta">
+        <slot></slot>
+    </div>
+</template>
+
+<style>
+    .alerta {
+        text-align: center;
+        margin-bottom: 1.5rem;
+        color: var(--error);
+        text-transform: uppercase;
+        font-weight: 700;
+        font-size: 1.8rem;
+        padding: .5rem
+    }
+</style>
